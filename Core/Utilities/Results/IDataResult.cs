620a334d34751e1cb461e3fa51dba715f491d270
Ali <alym11731996@gmail.com>
2022-04-09 16:17:26 +0300

    Under the core layer utilities transactions were made

---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<out T>:IResult //IDataResult'ın içinde IResult taki success ve message var dedim!
+    {
+        T Data { get; } //döndürmek istediğim data yı bununla döndüreceğim!
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult //IResult'ı implemente eden sınıfı oluşturdum, buradakileri genelde constructor üzerinden yönetiriz!
+    {
+
+        public Result(bool success, string message) : this(success) //böyle diyerek aşağıdaki constructorı da tetikliyorum! iki parametre gelirse ondan da success i alacak! //bu this ile bu constructor'ın içine tekrar success i yazmamış oldum!
+        {
+            this.Message = message;
+        }
+        public Result(bool success)
+        {
+            this.Success = success;
+        }
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
