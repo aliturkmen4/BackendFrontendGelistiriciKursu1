db500ffaa138092a913ae8fb487a4bb009c6bf4b
Ali <alym11731996@gmail.com>
2022-04-10 00:29:10 +0300

    web apı was creaated and autofac implementation was made in program.cs

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module //burada module yazıp using.autofac yaptım bu bize overrideble bir ortam sunuyor!
+    {
+        protected override void Load(ContainerBuilder builder) //adı üstünde yükle(konfigürasyonu yaptığımız yer olacak!)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>(); //productmanager tipimi register et dedim! //bu şey demek birisi IProductService şeklinde bir isterse constructorında  biz ona ProductManager veriyor olacağız!
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,113 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService; //dependency injection için field ekledim!
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")] //tüm ürünleri getirmek için!
+        public IActionResult GetList()
+        {
+            var result = _productService.GetList();
+
+            if (result.Success) //burada result yönetimi yaptım!
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpGet("getlistbycategory")] //category ye göre ürünleri getirmek için!
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _productService.GetListByCategory(categoryId);
+
+            if (result.Success) 
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpGet("getbyid")] //tek bir ürün döndüreceğim id 'ye göre çekebilirim!
+        public IActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+
+            if (result.Success) //burada result yönetimi yaptım!
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("add")] //ekleme benim için post methodu!
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+
+            if (result.Success) //burada result yönetimi yaptım!
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("update")] //güncelleme benim için post methodu!
+        public IActionResult Update(Product product)
+        {
+            var result = _productService.Update(product);
+
+            if (result.Success) 
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+
+        [HttpPost("delete")] //silme benim için post methodu!
+        public IActionResult Delete(Product product)
+        {
+            var result = _productService.Delete(product);
+
+            if (result.Success) 
+            {
+                return Ok(result.Message);
+            }
+            else
+            {
+                return BadRequest(result.Message);
+            }
+        }
+    }
+}
