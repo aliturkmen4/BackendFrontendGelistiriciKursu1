12616b640b914009d6fa3aafec614049b9b22f26
Ali <alym11731996@gmail.com>
2022-04-09 17:44:21 +0300

    successresult and successdataresult necessary implementing codes were made in the core layer

---
+++ Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        public SuccessDataResult(T data) : base(data, true)
+        {
+        }
+
+        public SuccessDataResult(T data, string message) : base(data, true, message) //zaten başarılı işlemi anlattığım için success vermeye gerek yok! success yerine true geçiyorum!
+        {
+        }
+
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+
+        }
+
+        public SuccessDataResult() : base(default, true)
+        {
+
+        }
+    }
+}
---
+++ Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult() : base(true) //parametresiz olan success i gönderirsem sadece true yu gönderdim!
+        {
+        }
+
+        public SuccessResult( string message) : base(true,message) //burada işlemin başarılı olduğunu söyleyerek sadece mesaj geçmek istedim!
+        {
+        }
+    }
+}
