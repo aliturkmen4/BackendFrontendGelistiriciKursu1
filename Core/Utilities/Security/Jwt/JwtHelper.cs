507817dc6c5f8d93730cee2af36b5c169fe03734
Ali <alym11731996@gmail.com>
2022-04-10 22:12:31 +0300

    securitykeyhelper was written

---
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encyription;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get;}
+        private TokenOptions _tokenOptions;
+
+        public JwtHelper(IConfiguration configuration) //configuration dosyası vasıtasıyla token option bilgilerini okumak için yazdım!
+        {
+            this.Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+        }
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        }
+    }
+}
---
+++ Core/Utilities/Security/Jwt/TokenOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class TokenOptions //apimizde setting içinde bilgi olarak tutacağız!
+    {
+        public string Audience { get; set; }
+
+        public string Issuer { get; set; }
+
+        public int AccessTokenExpiration { get; set; } //dakika cinsinden yazdığım için int!
+
+        public string SecurityKey { get; set; }
+    }
+}
